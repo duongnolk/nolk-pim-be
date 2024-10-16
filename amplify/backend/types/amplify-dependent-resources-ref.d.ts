ebeb7771bbb3490ce827ae26ffa285fceab4f94c
duongdaever <duong@nolk.com>
2024-10-17 03:23:50 +0700

    add function brand, sku, bundle

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "nolkPimBeBrand": {
+    "nolkpimbe": {
       "ApiId": "string",
       "ApiName": "string",
       "RootUrl": "string"
@@ -8,6 +8,20 @@ export type AmplifyDependentResourcesAttributes = {
   },
   "function": {
     "NolkPimBeBrand": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "NolkPimBeBundle": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "NolkPimBeSku": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
       "LambdaExecutionRoleArn": "string",
