c08b92414045fa93acbf0cb5ed50bcb6790b627f
duongdaever <duong@nolk.com>
2024-10-18 02:30:19 +0700

    Filter by name and API download file

---
+++ amplify/backend/function/NolkPimBeBrand/src/df_columns.py
@@ -0,0 +1,15 @@
+# DF COLUMNS
+brands = [
+    'brand_id',
+    'code',
+    'name',
+    'description',
+    'abbreviation',
+    'logo_image_url',
+    'status',
+    'stage',
+    'group',
+    'created_at',
+    'updated_at',
+    'deleted_at'
+]
